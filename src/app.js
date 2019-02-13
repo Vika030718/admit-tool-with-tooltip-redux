2f1f0317560a3539d6f70335f5c9a39ea0fcddcb
viktoria.matveeva <vika030718@gmail.com>
2019-02-13 15:48:18 +0200

    add gallery to index

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import React from 'react';
 import 'bootstrap';
 import './scss/app.scss';
 import {HashRouter, Route, Switch, Redirect} from 'react-router-dom';
-import {HomePage} from './components/mainPages/HomePage';
 import {AboutPage} from './components/mainPages/AboutPage';
 import {GalleryPage} from './components/mainPages/GalleryPage';
 import {Whoops404Page} from './components/mainPages/Whoops404Page';
@@ -13,7 +12,7 @@ render(
     <HashRouter>
       <div className="main">
         <Switch>
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={GalleryPage} />
           <Route path="/gallery" component={GalleryPage} />
           <Redirect from="/history" to="/about/history" />
           <Redirect from="/services" to="/about/services" />
